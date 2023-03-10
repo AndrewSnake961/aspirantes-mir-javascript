66c2211e5f337de6b22002952ce0fe777a48af19
Andres <felipe.hbuiles@gmail.com>
2023-03-10 18:58:53 -0500

    hello.js added and working

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+// escribe la función hola acá
+function hola(string){
+    return "Hola " + string + "!"
+}
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
