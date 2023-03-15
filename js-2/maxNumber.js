fa26d819ecb59d9969b695ed47ced41549ae8c77
Andres <felipe.hbuiles@gmail.com>
2023-03-14 22:09:45 -0500

    MaxNumber file added and working

---
+++ js-2/maxNumber.js
@@ -0,0 +1,23 @@
+// Crea una función max que reciba un arreglo de números y 
+// retorne el número máximo sin usar el método Math.max de 
+// JavaScript. Si el arreglo está vacío debe retornar undefined
+
+//escribe la función max acá
+
+function max(arrayOfNumbers){
+    if(arrayOfNumbers.length==0){
+        return undefined
+    }
+    let maxNumber=0
+    for(let i=0; i<arrayOfNumbers.length; i++){
+        if(maxNumber<arrayOfNumbers[i]){
+            maxNumber = arrayOfNumbers[i]
+        }
+    }
+    return maxNumber
+
+}
+
+console.log(max([1, 3, 2])) // 3
+console.log(max([10, 9, 8, 7, 6, 5, 4])) // 10
+console.log(max([])) // undefined
