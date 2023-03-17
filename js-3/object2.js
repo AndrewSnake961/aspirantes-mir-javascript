e15eea70a97028f931ba194983695ee7e79e1319
Andres <felipe.hbuiles@gmail.com>
2023-03-16 19:57:00 -0500

    solucion a los ejercicios de js

--- js-3/object2.js
+++ js-3/object2.js
@@ -1,3 +1,5 @@
+/*Crea un objeto literal llamado "persona" con las siguientes 
+propiedades: nombre, edad, ciudad y profesión.*/
 const persona ={
     nombre:"Andres" ,
     edad: 27,
@@ -5,18 +7,26 @@ const persona ={
     profesion: "Ingeniero Mecánico",
 
 }
+//Imprime en la consola el objeto "persona" completo
 console.log(persona)
-//Crea una función llamada "presentacion" que tome el objeto 
-//"persona" como argumento y devuelva una cadena de texto que 
-//incluya el nombre, la edad y la ciudad de la persona.
 
+/*Crea una función llamada "presentacion" que tome el 
+objeto "persona" como argumento y devuelva una cadena de texto
+que incluya el nombre, la edad y la ciudad de la persona*/
 function presentacion(person){
     return `Hola mi nomrbe es ${person.nombre}, tengo ${person.edad} años,y actualmente vivo en ${person.ciudad}.`
 }
+//Llama a la función "presentacion" y almacena el resultado en una variable llamada "mensaje"
 mensaje=presentacion(persona)
+//Imprime en la consola el valor de la variable "mensaje".
 console.log(mensaje)
+
+/*Agrega una nueva propiedad al objeto "persona" llamada
+ "hobbies" que sea un arreglo con algunos pasatiempos.*/
 persona.hobbies = ["Videojuegos","Natación","Leer","Ver series de sci-fi"]
+//Imprime en la consola la propiedad "hobbies" del objeto "persona".
 console.log(persona.hobbies)
+//Utiliza un ciclo for para imprimir en la consola cada uno de los elementos del arreglo de hobbies.
 for(let i of persona.hobbies){
     console.log(i)
 }
--- js-3/object3.js
+++ js-3/object3.js
@@ -1,15 +1,23 @@
+/*Crear una variable llamada receta e inicialízalo con un objeto literal vacío.*/ 
 const receta={
 
 }
-console.log(receta)
+//Agrégale una propiedad nombre con valor “Sandwich”.
 receta.nombre = "Sandwich"
+//Agrégale una propiedad ingredientes con un arreglo vacío.
 receta.ingredientes = []
-console.log(receta)
+/*Agrega un objeto al arreglo ingredientes con las siguientes propiedades:
+nombre: “Pan”
+cantidad: 2 */
 receta.ingredientes.push({nombre: "Pan", cantidad: 2})
+/**Agrega otro ingrediente al arreglo con las siguientes propiedades:
+nombre: “Queso”
+cantidad: 1 */
 receta.ingredientes.push({nombre: "Queso",cantidad: 1})
+/**Debajo del objeto imprime en la consola el nombre del primer ingrediente, debería aparecer "Pan". */
 console.log(receta.ingredientes[0].nombre)
+
+/**Suma todos las cantidades de los ingredientes y muestra el resultado en consola. */
 quantityOfIngredients= receta.ingredientes[0].cantidad + receta.ingredientes[1].cantidad
 console.log(`La cantidad de ingredientes utilizados es ${quantityOfIngredients}`)
 
-// Suma todos las cantidades de los ingredientes y muestra 
-//el resultado en consola.
